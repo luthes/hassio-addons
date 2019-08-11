23c65f021c66cc0e238985ba27c9e10b5718e393
zer0 <steve.luther@gmail.com>
2019-08-11 16:17:07 -0700

    adding some debug stuff

--- rootfs/usr/bin/run.sh
+++ rootfs/usr/bin/run.sh
@@ -12,6 +12,12 @@ export AWS_SECRET_ACCESS_KEY=$(bashio::config 'aws_secret_key')
 # Bucket to Sync to 
 BUCKET=$(bashio::config 'aws_s3_bucket')
 
+bashio::log.info "Checking Log Directories"
+cd / && ls -lsa
+
+# Run our Sync Command -- We should make sure that we can
+# Error if no internet or something
+
 bashio::log.info "Running command: aws s3 sync /backup/ s3://$BUCKET/"
 
 # Run our Sync Command -- We should make sure that we can
