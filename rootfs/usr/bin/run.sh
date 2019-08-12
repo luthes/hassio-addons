7a9782df8d0a474daa9302623f858d8ffa7f3912
zer0 <steve.luther@gmail.com>
2019-08-11 17:31:11 -0700

    remove some useless debug stuff

--- rootfs/usr/bin/run.sh
+++ rootfs/usr/bin/run.sh
@@ -1,31 +1,25 @@
 #!/usr/bin/with-contenv bashio
-#========================================
-#
-#    Sync Hass.io Backups to S3. 
-#
-#========================================
+#=======================================#
+#                                       #
+#      Sync Hass.io Backups to S3.      #
+#                                       #
+#=======================================#
 
-# AWS Variables
+# AWS Credentials
 export AWS_ACCESS_KEY_ID=$(bashio::config 'aws_access_key')
 export AWS_SECRET_ACCESS_KEY=$(bashio::config 'aws_secret_key')
 
 # Bucket to Sync to 
 BUCKET=$(bashio::config 'aws_s3_bucket')
 
-# Some Debug stuff
-bashio::log.info "Checking Log Directories"
-bashio::log.info "$(cd / && ls -lsa)"
-
-bashio::log.info "Checking data directory"
-bashio::log.info "$(cd / && ls -lsa /data)"
-
-bashio::log.info "Find Backup Directory"
-bashio::log.info "$(cd / && find . -type d -name 'backup')"
-
-bashio::log.info "Find Backup Files"
-bashio::log.info "$(cd / && find / -type f -name '92b87d6d.tar')"
+bashio::log.debug "Find Backup Directory"
+bashio::log.debug "Found: $(cd / && find . -type d -name 'backup')"
 
 # Run our Sync Command -- We should make sure that we can
 # Error if no internet or something
 bashio::log.info "Running command: aws s3 sync /backup/ s3://$BUCKET/"
-aws s3 sync /backup/ s3://$BUCKET/
+aws s3 sync /backup/ s3://$BUCKET/ > /dev/null
+
+bashio::log.info "Contents of $FOLDER backed up to s3://$BUCKET"
+aws s3 sync /backup/ s3://$BUCKET/ > /dev/null
+
